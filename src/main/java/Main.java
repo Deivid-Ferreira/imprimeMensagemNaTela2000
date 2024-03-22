8665fd9635f3ca5f0d2dcc696b9afd6a05f3311a
deividferreira0 <32979859-deividferreira0@users.noreply.replit.com>
2024-03-22 00:04:41 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+
+
+// Programa imprime uma mensagem na tela
+// Autor : Deivid Ferreira
+
+
+public class Main {
+  public static void main(String[] args) {
+    // System.out.println("Hello world!");
+
+  //variaveis do tipo boolrana
+    boolean media  = true;
+    boolean frequencia = true;
+    boolean aprovado = media && frequencia;
+    boolean cafe = false;
+    boolean agua = true;
+    boolean clienteServido = cafe || agua;  
+    int idade = 18;  
+    double salario = 1000.0;
+    float temperatura = 25.5f;
+    //Strings do tipo texto
+    String nome = "Deivid";
+    String cpf = "12345678910";
+    String aniversario = "01/01/2000";
+
+    
+// Metodo de saida
+    // System.out.println("Aprovação é "+aprovado);
+    // System.out.println("Cliente servido ? " +clienteServido);
+  }
+}
